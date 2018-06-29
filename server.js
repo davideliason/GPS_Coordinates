1129441f8ca29e303b0d022eb5420faa9a0821c7
David Eliason <david@davethemaker.com>
2018-06-29 14:54:59 -0700

    successfully respond to /sample route with a JSON object containing array of two objects of sample data

--- server.js
+++ server.js
@@ -13,7 +13,18 @@ const app = express()
 app.get('/', (req, res) => res.send('Hello World! :)'))
 
 app.get('/sample', function (req, res, next) {
-    res.send('sample works');
+    // res.send('sample works');  simple response
+    res.json([{
+        id: 1,
+        username: "john",
+        latitude: "here",
+        longitude: "there"
+    }, {
+        id: 2,
+        username: "bob",
+        latitude: "beach",
+        longitude: "beachtowel"
+    }]);
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
