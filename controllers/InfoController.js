3727d089de4643dcdacd92040be3c2acab426946
David Eliason <david@davethemaker.com>
2018-06-29 18:57:19 -0700

    create route for creating new Info

--- controllers/InfoController.js
+++ controllers/InfoController.js
@@ -8,3 +8,13 @@ exports.listAllInfos = (req, res) => {
         res.status(200).json(info);
     });
 };
+
+exports.createNewInfo = (req, res) => {
+    let newInfo = new Info(req.body);
+    newInfo.save((err, info) => {
+        if (err) {
+            res.status(500).send(err);
+        }
+        res.status(201).json(info);
+    });
+};
