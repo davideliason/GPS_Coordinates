45a8a978f8847e53760728e2bf9190ccc6aebacc
David Eliason <david@davethemaker.com>
2018-06-30 09:38:22 -0700

    researched on security and added helmet.js for a start

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
+var helmet = require('helmet')
 
 require('dotenv').config()
 // instantiate express
 var app = express();
 var port = process.env.PORT || '3001';
 // log request details
-app.use(morgan('combined'))
+app.use(morgan('combined'));
+// help with security by setting HTTP headers
+// X-Powered-By header disabled by helmet.js
+app.use(helmet());
 
 
 app.get('/', (req, res) => {
