7feafedc6f7be2ffe46983b31659dd5735a538e6
David Eliason <david@davethemaker.com>
2018-06-29 18:48:33 -0700

    set up middleware body-parser

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ require("./config/db");
 const app = express();
 const port = process.env.PORT || 3301;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => res.send('Hello World! :)'))
 
 app.get('/users', function (req, res, next) {
