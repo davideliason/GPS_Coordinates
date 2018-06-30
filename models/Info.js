89cfc7c174319c4b63ff8e3c934243f094ce1898
David Eliason <david@davethemaker.com>
2018-06-29 18:20:53 -0700

    add schema for Info with name, date, lat and long info

---
+++ models/Info.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const InfoSchema = new Schema({
+    infoName: {
+        type: String,
+        required: true
+    },
+    createdOn: {
+        type: Date,
+        default: Date.now
+    },
+    infoLatitude: {
+        type: String,
+        required: true
+    }
+    infoLongitude: {
+        type: String,
+        required: true
+    }
+});
