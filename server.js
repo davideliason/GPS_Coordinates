37693923f4aa2eba905ed29ba57651e4f20ccc14
David Eliason <david@davethemaker.com>
2018-06-30 07:48:10 -0700

    install and config body-parser, ready for front-end body parsing

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 
 const express = require("express");
+const bodyParser = require("body-parser");
+
 require('dotenv').config()
 require("./config/db");
 
 const app = express();
 const port = process.env.PORT || 3301;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
